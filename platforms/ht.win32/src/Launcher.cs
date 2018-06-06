5d20b0476eb0eb90ecb721f13f957db24540e6c5
Bastian Blokland <bastian@armadainteractive.com>
2018-06-07 00:13:41 +0300

    Created skeleton Win32 project

---
+++ platforms/ht.win32/src/Launcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HT.Win32
+{
+    public static class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Win32] Launching program..");
+
+            using(var app = new NativeApp())
+            {
+                HT.Main.Program.Run(app);
+            }
+
+            Console.WriteLine("[Win32] Program terminated");
+        }
+    }
+}
