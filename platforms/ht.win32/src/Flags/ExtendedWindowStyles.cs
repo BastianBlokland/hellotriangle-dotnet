6f389d131752fa1b123156b56638efd6f6455613
Bastian Blokland <bastian@armadainteractive.com>
2018-06-07 23:43:46 +0300

    Minor code cleanup in ht.win32 project

--- platforms/ht.win32/src/Flags/ExtendedWindowStyles.cs
+++ platforms/ht.win32/src/Flags/ExtendedWindowStyles.cs
@@ -10,49 +10,27 @@ namespace HT.Win32.Flags
     {
         Default = 0x00000000, //Same as 'Left'
         AcceptFiles = 0x00000010,
-
         AppWindow = 0x00040000,
-
         ClientEdge = 0x00000200,
-
         Composited = 0x02000000,
-
         ContextHelp = 0x00000400,
-
         ControlParent = 0x00010000,
-
         DLGModalFrame = 0x00000001,
-
         Layered = 0x00080000,
-
         LayoutRTL = 0x00400000,
-
         Left = 0x00000000,
-
         LeftScrollbar = 0x00004000,
-
         LTRReading = 0x00000000,
-
         MDIChild = 0x00000040,
-
         NoActivate = 0x08000000,
-
         NoInheritLayout = 0x00100000,
-
         NoParentNotify = 0x00000004,
-
         NoRedirectionBitmap = 0x00200000,
-
         OverlappedWindow = WindowEdge | ClientEdge,
-
         PaletteWindow = WindowEdge | ToolWindow | TopMost,
-
         Right = 0x00001000,
-
         RightScrollbar = 0x00000000,
-
         RTLReading = 0x00002000,
-
         StaticEdge = 0x00020000,
         ToolWindow = 0x00000080,
         TopMost = 0x00000008,
--- platforms/ht.win32/src/NativeApp.cs
+++ platforms/ht.win32/src/NativeApp.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 using HT.Engine.Math;
 
