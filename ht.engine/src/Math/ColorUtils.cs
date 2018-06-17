d30da62d6ffa38d91a5de9674b848dab795b0c2d
Bastian Blokland <bastianblokland@gmail.com>
2018-06-17 11:04:06 +0300

    Add color presets for all the web-colors

---
+++ ht.engine/src/Math/ColorUtils.cs
@@ -0,0 +1,22 @@
+namespace HT.Engine.Math
+{
+    public static class ColorUtils
+    {
+        public static readonly Float4 White = new Float4(1f, 1f, 1f, 1f);
+        public static readonly Float4 Silver = new Float4(.75f, .75f, .75f, 1f);
+        public static readonly Float4 Gray = new Float4(.5f, .5f, .5f, 1f);
+        public static readonly Float4 Black = new Float4(0f, 0f, 0f, 1f);
+        public static readonly Float4 Red = new Float4(1f, 0f, 0f, 1f);
+        public static readonly Float4 Maroon = new Float4(.5f, 0f, 0f, 1f);
+        public static readonly Float4 Yellow = new Float4(1f, 1f, 0f, 1f);
+        public static readonly Float4 Olive = new Float4(.5f, .5f, 0f, 1f);
+        public static readonly Float4 Lime = new Float4(0f, 1f, 0f, 1f);
+        public static readonly Float4 Green = new Float4(0f, .5f, 0f, 1f);
+        public static readonly Float4 Aqua = new Float4(0f, 1f, 1f, 1f);
+        public static readonly Float4 Teal = new Float4(0f, .5f, .5f, 1f);
+        public static readonly Float4 Blue = new Float4(0f, 0f, 1f, 1f);
+        public static readonly Float4 Navy = new Float4(0f, 0f, .5f, 1f);
+        public static readonly Float4 Fuchsia = new Float4(1f, 0f, 1f, 1f);
+        public static readonly Float4 Purple = new Float4(.5f, 0f, .5f, 1f);
+    }
+}
