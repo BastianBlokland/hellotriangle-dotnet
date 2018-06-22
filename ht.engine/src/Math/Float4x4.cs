0939438ae2535a40b5b1a10c377d08184df6655d
Bastian Blokland <bastianblokland@gmail.com>
2018-06-22 12:59:41 +0300

    Base Float4x4 matrix implementation

---
+++ ht.engine/src/Math/Float4x4.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace HT.Engine.Math
+{
+    [StructLayout(LayoutKind.Sequential, Size = SIZE)]
+    public struct Float4x4 : IEquatable<Float4x4>
+    {
+        public const int SIZE = Float4.SIZE * 4;
+
+        //Presets
+        public static readonly Float4x4 Identity = Float4x4.FromRows(
+            row0: (1f, 0f, 0f, 0f),
+            row1: (0f, 1f, 0f, 0f),
+            row2: (0f, 0f, 1f, 0f),
+            row3: (0f, 0f, 0f, 1f));
+
+        //Helper properties
+        public Float3 Translation => Column3.XYZ;
+
+        //Column index accessor
+        public Float4 this[int i]
+        {
+            get 
+            {
+                switch(i)
+                {
+                    case 0: return Column0;
+                    case 1: return Column1;
+                    case 2: return Column2;
+                    case 3: return Column3;
+                }
+                throw new ArgumentOutOfRangeException(
+                    $"[{nameof(Float4x4)}] No column at: {i}", nameof(i));
+            }
+        }
+
+        //Alternative representation
+        public Float4 Row0 => new Float4(Column0.X, Column1.X, Column2.X, Column3.X);
+        public Float4 Row1 => new Float4(Column0.Y, Column1.Y, Column2.Y, Column3.Y);
+        public Float4 Row2 => new Float4(Column0.Z, Column1.Z, Column2.Z, Column3.Z);
+        public Float4 Row3 => new Float4(Column0.W, Column1.W, Column2.W, Column3.W);
+
+        //Data
+        public readonly Float4 Column0;
+        public readonly Float4 Column1;
+        public readonly Float4 Column2;
+        public readonly Float4 Column3;
+
+        public Float4x4(Float4 column0, Float4 column1, Float4 column2, Float4 column3)
+        {
+            Column0 = column0;
+            Column1 = column1;
+            Column2 = column2;
+            Column3 = column3;
+        }
+
+        //Creation
+        public static Float4x4 FromRows(Float4 row0, Float4 row1, Float4 row2, Float4 row3)
+            => new Float4x4(
+                column0: (row0.X, row1.X, row2.X, row3.X),
+                column1: (row0.Y, row1.Y, row2.Y, row3.Y),
+                column2: (row0.Z, row1.Z, row2.Z, row3.Z),
+                column3: (row0.W, row1.W, row2.W, row3.W));
+
+        //Equality
+        public static bool operator ==(Float4x4 a, Float4x4 b) => a.Equals(b);
+
+        public static bool operator !=(Float4x4 a, Float4x4 b) => !a.Equals(b);
+
+        public override bool Equals(object obj) => obj is Float4x4 && Equals((Float4x4)obj);
+
+        public bool Equals(Float4x4 other) => 
+            other.Column0 == Column0 &&
+            other.Column1 == Column1 &&
+            other.Column2 == Column2 &&
+            other.Column3 == Column3;
+
+        public override int GetHashCode() => 
+            Column0.GetHashCode() ^
+            Column1.GetHashCode() ^
+            Column2.GetHashCode() ^
+            Column3.GetHashCode();
+
+        public override string ToString() => 
+$@"(
+    Row0: {Row0} 
+    Row1: {Row1}
+    Row2: {Row2}
+    Row3: {Row3}
+)";
+    }
+}
